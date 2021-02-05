79b7e952e208595e6d2b04e807355dc5b16d289c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 08:32:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,12 +28,13 @@
 def line 
   if katz_deli.size == 0 
     "The line is currently empty."
-  else 
-    i = 0 
-    while i < katz_deli.size
-      "The line is currently: #{i + 1}. #{katz_deli[i]}"
-      i += 1 
-    end
+  # else 
+  #   i = 0
+  #   initial_string = 
+  #   while i < katz_deli.size
+  #     "The line is currently: #{i + 1}. #{katz_deli[i]}"
+  #     i += 1 
+  #   end
   end
 end
 
