63281bc05b3622fc3c50051532c5fbe33f865838
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 08:45:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -47,8 +47,9 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
-  else katz_deli.shift
+  else 
     puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   end
 end
 
